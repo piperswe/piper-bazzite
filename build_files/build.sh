812bc5ecb79f75606a10d1b68e18872d89e0c129
Piper McCorkle <contact@piperswe.me>
2026-03-06 15:18:15 -0600

    install packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,19 @@
 
 set -ouex pipefail
 
-### Install packages
+dnf5 -y config-manager addrepo --from-repofile https://pkg.cloudflare.com/cloudflared.repo
+dnf5 -y config-manager addrepo --from-repofile https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo
+dnf5 -y config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
+dnf5 copr enable jdxcode/mise
+dnf5 install -y \
+    cloudflared \
+    cloudflare-warp \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin \
+    mise \
+    zed
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+systemctl enable docker
