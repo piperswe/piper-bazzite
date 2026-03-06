96651df0b00fa0ec063ac2fa521782789deb1453
Piper McCorkle <contact@piperswe.me>
2026-03-06 15:27:40 -0600

    Enable Terra

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,8 +5,9 @@ set -ouex pipefail
 dnf5 -y config-manager addrepo --from-repofile https://pkg.cloudflare.com/cloudflared.repo
 dnf5 -y config-manager addrepo --from-repofile https://pkg.cloudflareclient.com/cloudflare-warp-ascii.repo
 dnf5 -y config-manager addrepo --from-repofile https://download.docker.com/linux/fedora/docker-ce.repo
-dnf5 copr enable jdxcode/mise
-dnf5 install -y \
+dnf5 -y copr enable jdxcode/mise
+dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+dnf5 -y install \
     cloudflared \
     cloudflare-warp \
     docker-ce \
